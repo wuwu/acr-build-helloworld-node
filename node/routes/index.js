77800be9f296bab27ea1663d73a2ae1b45a89b26
Florian Ewald <florian.ewald@sinnerschrader.com>
2020-11-12 21:19:11 +0100

    update file to trigger hook

--- node/routes/index.js
+++ node/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'my first container app' });
+  res.render('index', { title: 'my first container app with commit hook' });
 });
 
 module.exports = router;
