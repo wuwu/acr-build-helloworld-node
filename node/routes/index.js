b0453c495e19bfe233ebe560c909377f18abc78e
Florian Ewald <florian.ewald@sinnerschrader.com>
2020-11-12 22:31:05 +0100

    delete files

--- node/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'my first container app with commit hook' });
-});
-
-module.exports = router;
